bd8722904fcd0a7814b74c86f07828f1b7f60760
Supersonic56 <lucykiszczakphd@gmail.com>
2023-04-12 15:17:10 -0700

    Summary

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,41 @@
+window.alert("Hello World")
+
+var A = "Good morning"
+document.write(A);
+
+var B = "How are you doing?"
+document.write(B);
+
+//creating two variables
+var Sent1 = "This is the beginning of the string ";
+var Sent2 = "and this is the end of the string.";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+//expression
+3 + 3
+
+//statement or an expression in a statement
+document.write(3 + 3);
+
+// concatenating strings
+document.write("Al told Bea, \"take it back! or I will tell her!\"<br>\"Don't do it!\" Al responded.");
+
+document.write("\"Friday became a cool, wet afternoon..." + "Mary might haven\'t been waiting outside for you...\"" + "-Dr Zeuss")
+
+var C = "Concatenated " + "String"
+document.write(C)
+
+// assigning multiple variables in one statement
+var Apple = "red", Pear = "yellow", Plum = "purple", Berry = "blue";
+document.write(Plum); 
+
+// a function
+function myFunction() {
+    var phrase = "This is a phrase";
+    document.getElementById("Phrase_Text").innerHTML = phrase;
+}
+
+
+
